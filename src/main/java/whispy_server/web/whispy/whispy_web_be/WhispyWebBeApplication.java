bcbef99d5f16ba2450e2f90ada6f631ad8928906
hyohyeon08 <hyohyeonkim08@gmail.com>
2025-06-03 18:51:24 +0900

    feat: 스프링부트 기본 세팅

---
+++ src/main/java/whispy_server/web/whispy/whispy_web_be/WhispyWebBeApplication.java
@@ -0,0 +1,13 @@
+package whispy_server.web.whispy.whispy_web_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhispyWebBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhispyWebBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/whispy_server/web/whispy/whispy_web_be/WhispyWebBeApplicationTests.java
@@ -0,0 +1,13 @@
+package whispy_server.web.whispy.whispy_web_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhispyWebBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
