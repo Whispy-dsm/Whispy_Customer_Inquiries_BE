ea5c169aca3916fe69d6fbb5bd57ed6f67c06f1b
hyohyeon08 <hyohyeonkim08@gmail.com>
2025-06-20 08:30:21 +0900

    refactor (#8) : Add Anotation

--- src/main/java/whispy_server/web/whispy/whispy_web_be/domain/bug/presentation/dto/request/BugReportRequestDto.java
+++ src/main/java/whispy_server/web/whispy/whispy_web_be/domain/bug/presentation/dto/request/BugReportRequestDto.java
@@ -1,9 +1,6 @@
 package whispy_server.web.whispy.whispy_web_be.domain.bug.presentation.dto.request;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import whispy_server.web.whispy.whispy_web_be.domain.bug.domain.enums.BugTopic;
 
 public record BugReportRequestDto(
@@ -11,6 +8,7 @@ public record BugReportRequestDto(
         @Size(max = 50, message = "제목은 50글자 이내로 해주세요.")
         String title,
 
+        @NotNull(message = "버그 유형을 선택해 주세요")
         BugTopic bugTopic,
 
         @NotBlank(message = "본문을 입력해 주세요")
